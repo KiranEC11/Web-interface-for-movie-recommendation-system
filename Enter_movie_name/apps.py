a9dd78fd0109a627aedeb4133cc7119cae48b72f
KiranEC11 <22m1514.iitb@gmail.com>
2023-07-25 04:19:01 +0530

    del

--- Enter_movie_name/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EnterMovieNameConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "Enter_movie_name"
--- Enter_movie_name/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns=[
-    path("", views.TitleView, name= "enter_title"),
-    path("recommendations/", views.TitleView, name='recommendations'),
-    
-]
